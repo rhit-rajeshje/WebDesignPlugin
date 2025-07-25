f5ef6ad0a017f1061942018419de7942f11b4047
rajeshje <rajeshje@rose-hulman.edu>
2025-07-25 14:55:32 -0400

    First Changes Made

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,53 @@ import "./style.css";
 const searchParams = new URLSearchParams(window.location.search);
 document.body.dataset.theme = searchParams.get("theme") ?? "light";
 
-document.querySelector("[data-handler='create-text']")?.addEventListener("click", () => {
+// document.querySelector("[id = 'img1']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img1')?.dataset.handler, "*");
+// });
+// document.querySelector("[id = 'img2']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img2')?.dataset.handler, "*");
+// });
+// document.querySelector("[id = 'img3']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img3')?.dataset.handler, "*");
+// });
+// document.querySelector("[id = 'img4']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img4')?.dataset.handler, "*");
+// });
+// document.querySelector("[id = 'img5']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img5')?.dataset.handler, "*");
+// });
+// document.querySelector("[id = 'img6']")?.addEventListener("click", () => {
+//   // send message to plugin.ts
+//   parent.postMessage(document.getElementById('img6')?.dataset.handler, "*");
+// });
+document.querySelector("[id = 'img7']")?.addEventListener("click", () => {
   // send message to plugin.ts
-  parent.postMessage("create-text", "*");
+  parent.postMessage(document.getElementById('img7')?.dataset.handler, "*");
 });
+document.querySelector("[id = 'img8']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage(document.getElementById('img8')?.dataset.handler, "*");
+});
+document.querySelector("[id = 'submit']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  const width = document.getElementById('submit')?.dataset.width;
+  const height = document.getElementById('submit')?.dataset.height;
+  const borderRadius = document.getElementById('submit')?.dataset.radius;
+  const fillColor = document.getElementById('submit')?.dataset.fillcolor;
+  const iconColor = document.getElementById('submit')?.dataset.iconcolor;
+  const version = document.getElementById('submit')?.dataset.version;
+
+  const dataMap = new Map([["data-width",width],["data-height",height],["data-borderRadius",borderRadius],["data-fillColor",fillColor],["data-iconColor",iconColor],["data-version",version]]);
+
+  parent.postMessage(dataMap, "*");
+  console.table(dataMap);
+});
+
 
 // Listen plugin.ts messages
 window.addEventListener("message", (event) => {
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,137 @@
 penpot.ui.open("Penpot plugin starter template", `?theme=${penpot.theme}`);
 
-penpot.ui.onMessage<string>((message) => {
-  if (message === "create-text") {
-    const text = penpot.createText("Hello world!");
+penpot.ui.onMessage<Map<string,string>>((message) => {
+  console.log("plugin");
+  const width = Number(message.get("data-width"));
+  const height = Number(message.get("data-height"));
+  const borderRadius = Number(message.get("data-borderRadius"));
+  const fillColor = message.get("data-fillColor");
+  const iconColor = message.get("data-iconColor");
+  if (message.get("data-version")=="1") {
+    const buttonBoard = penpot.createBoard();
+    buttonBoard.resize(width,height);
+    buttonBoard.x = penpot.viewport.center.x;
+    buttonBoard.y = penpot.viewport.center.y;
+    buttonBoard.borderRadius = borderRadius;
 
-    if (text) {
-      text.x = penpot.viewport.center.x;
-      text.y = penpot.viewport.center.y;
+    const button = penpot.createRectangle();
+    button.resize(width,height);
+    button.x = penpot.viewport.center.x;
+    button.y = penpot.viewport.center.y;
+    button.borderRadius = borderRadius;
+    button.strokes = [{strokeColor: fillColor, strokeWidth: 15}]
+    button.fills = [{fillColor: iconColor}]
+   
+    const buttonText = penpot.createText("Button");
+    if(buttonText){
+    buttonText.x = penpot.viewport.center.x+35;
+    buttonText.y = penpot.viewport.center.y+23;
+    buttonText.fontSize= '45';
+    buttonText.fontFamily = 'Sour Gummy';
+    buttonText.fills = [{fillColor: fillColor}];
+    buttonBoard.insertChild(0, buttonText);
+  }
+    buttonBoard.insertChild(0, button);
+  
+    const buttonBoard2 = penpot.createBoard();
+    buttonBoard2.resize(width,height);
+    buttonBoard2.x = penpot.viewport.center.x;
+    buttonBoard2.y = penpot.viewport.center.y;
+    buttonBoard2.borderRadius = borderRadius;
 
-      penpot.selection = [text];
+    const button2 = penpot.createRectangle();
+    button2.resize(width,height);
+    button2.x = penpot.viewport.center.x;
+    button2.y = penpot.viewport.center.y;
+    button2.borderRadius = borderRadius;
+    button2.fills = [{fillColor: fillColor}]
+
+    const buttonText2 = penpot.createText("Button");
+    if(buttonText2){
+    buttonText2.x = penpot.viewport.center.x+35;
+    buttonText2.y = penpot.viewport.center.y+23;
+    buttonText2.fontSize= '45';
+    buttonText2.fontFamily = 'Sour Gummy';
+    buttonText2.fills = [{fillColor: iconColor}];
+    buttonBoard2.insertChild(0, buttonText2);
+    }
+    buttonBoard2.insertChild(0, button2);
+    buttonBoard2.addInteraction('mouse-enter', {type: 'navigate-to', destination: buttonBoard});
+    buttonBoard.addInteraction('mouse-leave', {type: 'navigate-to', destination: buttonBoard2});
+
+    penpot.selection = [buttonBoard, buttonBoard2];
+    const buttonStyle1 = penpot.group(penpot.selection);
+    if(buttonStyle1){
+      buttonStyle1.name = "Button Style 1";
     }
   }
+
+  if(message.get("data-version")=="2"){
+    const circleBoard = penpot.createBoard();
+    circleBoard.x = penpot.viewport.center.x;
+    circleBoard.y = penpot.viewport.center.y;
+    circleBoard.borderRadius = borderRadius;
+    circleBoard.resize(width,height);
+    circleBoard.name = "Button (Hovered)";
+
+    const circle = penpot.createRectangle();
+    circle.x = penpot.viewport.center.x;
+    circle.y = penpot.viewport.center.y;
+    circle.borderRadius = borderRadius;
+    circle.resize(width,height);
+    circle.fills = [{fillColor: iconColor}];
+    circle.strokes = [{strokeColor: fillColor, strokeWidth: 10}]
+    circle.name = "Circle (Hovered)";
+
+
+    const arrowRight = penpot.createShapeFromSvg('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF8577" class="bi bi-caret-right-fill" viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/> </svg>');
+    if(arrowRight){
+    arrowRight.x = penpot.viewport.center.x+width/4;
+    arrowRight.y = penpot.viewport.center.y+height/5;
+    arrowRight.resize(width/2,60);
+    circleBoard.insertChild(0,arrowRight);
+    arrowRight.name = "Arrow (Hovered)";
+    }
+    circleBoard.insertChild(0,circle);
+
+    const circleBoard2 = penpot.createBoard();
+    circleBoard2.x = penpot.viewport.center.x;
+    circleBoard2.y = penpot.viewport.center.y;
+    circleBoard2.borderRadius = borderRadius;
+    circleBoard2.resize(width,height);
+    circleBoard2.name = "Button";
+
+    const circle2 = penpot.createRectangle();
+    circle2.x = penpot.viewport.center.x;
+    circle2.y = penpot.viewport.center.y;
+    circle2.borderRadius = borderRadius;
+    circle2.resize(width,height);
+    circle2.fills = [{fillColor: fillColor}];
+    circle2.name = "Circle";
+
+    
+    const arrowRight2 = penpot.createShapeFromSvg('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF" class="bi bi-caret-right-fill" viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/> </svg>');
+    if(arrowRight2){
+    arrowRight2.x = penpot.viewport.center.x+width/4;
+    arrowRight2.y = penpot.viewport.center.y+height/5;
+    arrowRight2.resize(width/2,60);
+    circleBoard2.insertChild(0,arrowRight2);
+    arrowRight2.name = "Arrow";
+    }
+
+    circleBoard2.insertChild(0,circle2);
+    circleBoard2.addInteraction('mouse-enter', {type: 'navigate-to', destination: circleBoard});
+    circleBoard.addInteraction('mouse-leave', {type: 'navigate-to', destination: circleBoard2});
+
+    penpot.selection = [circleBoard,circleBoard2];
+    const buttonStyle2 = penpot.group(penpot.selection);
+    if(buttonStyle2){
+      buttonStyle2.name = "Button Style 2";
+    }
+
+    
+  }
+
 });
 
 // Update the theme in the iframe
